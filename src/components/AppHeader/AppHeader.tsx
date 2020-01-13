ca126fe95234e4867d34063681a8d82a1c222a6f
Vitaly Evtekhov <gonzoligga@gmail.com>
2020-01-13 22:30:22 +0300

    Configure storybook css modules loader, fix class name

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import s from './AppHeader.module.css';
 
 export const AppHeader: React.FC = () => {
-    return <header className={s.Header}></header>;
+    return <header className={s.header}></header>;
 };
